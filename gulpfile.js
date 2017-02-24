d152cfd7b59f61422ccc058706bc4ca2ead55cfb
Cyril Mupakile <cmupakile@spartaglobal.co>
2017-02-24 15:49:42 +0000

    added background images, improvements to css optimisation and introduction of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,101 @@
 var gulp = require('gulp');
 
-gulp.task('default, function(){
+var sass = require('gulp-sass');
 
-//code for functions can go here
+var autoprefixer = require('gulp-autoprefixer');
 
+gulp.task('sass', function() {
+  return gulp.src('cv_document/scss/**/*.scss') // Gets all files ending with .scss in cv_document/scss
+    .pipe(sass())
+    .pipe(autoprefixer({browsers: ['last 3 versions']}))
+    .pipe(gulp.dest('cv_document/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
+
+
+
+
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'cv_document'
+    },
+  })
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('cv_document/scss/**/*.scss', ['sass']);
+  // reload browser when HTML or JS files changed
+  gulp.watch('cv_document/*html', browserSync.reload);
+  gulp.watch('cv_document/js/**/*.js', browserSync.reload);
+});
+
+
+
+
+// Collects js together - minify + put in dist folder
+var useref = require('gulp-useref');
+var cssnano = require('gulp-cssnano');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+
+gulp.task('useref', function(){
+  return gulp.src('cv_document/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+
+//image optimising
+var imagemin = require('gulp-imagemin');
+
+var cache = require('gulp-cache');
+
+gulp.task('images', function(){
+  return gulp.src('cv_document/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist../images'))
+});
+
+//Cleaning Up Generated files
+var del = require('del');
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+//Remove images from cache
+gulp.task('cache:clear', function (callback) {
+return cache.clearAll(callback)
+})
+
+
+
+// sass + watch + sync with 'gulp' command only
+gulp.task('default', function (callback) {
+  runSequence(['sass','browserSync', 'watch'],
+    callback
+  )
+})
+
+//run sequence
+
+var runSequence = require('run-sequence');
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref', 'images'],
+    callback
+  )
+})
